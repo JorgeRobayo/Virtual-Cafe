8cb686cb6b582af6f94edc71d1d708398d491b7c
jorge robayo <jorgerobmor@gmail.com>
2024-01-11 11:33:21 -0500

    working code

---
+++ notes.js
@@ -0,0 +1,12 @@
+//1 create react app
+//2 npm run build -> production
+//3 npm i express morgan serve-favicon
+//4 const express = require('express');
+    // const path = require('path');
+    // const favicon = require('serve-favicon');
+    // const logger = require('morgan');
+
+    // const app = express();
+
+    // app.use(logger('dev'));
+    // app.use(express.json());
